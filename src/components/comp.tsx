9d0f2db21a56744a6499cb3c5b9a8662c1f90c30
hayashi.yuhki <hayashi.yuhki@gmail.com>
2025-02-01 14:11:38 +0900

    show env button

--- src/components/comp.tsx
+++ src/components/comp.tsx
@@ -9,9 +9,8 @@ export default function Comp() {
   }
 
   return (
-    <div>
-      <h1>Hello, Astro!</h1>
-      <button onClick={()=>{buttonHandler("hello ")}}>Click me!</button>
+    <div className=" self-start flex justify-center items-center w-52 h-24 bg-blue-600 text-white px-6 py-3 rounded-full font-semibold shadow-md hover:bg-blue-500 cursor-pointer">
+      <button onClick={()=>{buttonHandler("hello ")}}>Show ENV Value from server !</button>
     </div>
   );
 }
