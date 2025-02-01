5f7fd1573a4ff8831b03bfa9dd6e2f04632de4be
hayashi.yuhki <hayashi.yuhki@gmail.com>
2025-02-01 13:19:01 +0900

    build script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        'react-dom/server': 'react-dom/server.edge',
-      },
-    },
+    // resolve: {
+    //   alias: {
+    //     'react-dom/server': 'react-dom/server.edge',
+    //   },
+    // },
   },
 });
